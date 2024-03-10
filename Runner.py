397c048f2a6f60dd7f52bf3c8aca2a3ce01a665c
Ivan Soregashi <8psycho@gmail.com>
2024-03-10 17:01:36 +0500

    More logging, couple of debug fuctions to rollback file movement. Also, Training folders with training jupyter files was added. New learning will happen there.

--- Runner.py
+++ Runner.py
@@ -11,9 +11,6 @@ from PIL import Image
 # 3 CONVERT SOME IMAGES/VIDEO TO REDUCE TRHE SIZE
 # 4 RENAME ARTIFACTS TO THE UNIFORM FORMAT PER PLATFORM AND CREATION TIME
 
-# CONFIG
-logging.basicConfig(filename='Runner.log', level=logging.DEBUG, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
 # CONSTANTS
 GDRIVE = Path.home() / 'My Drive'                                   #Contains: iOSAPP img, rec, andss, andvid, andlogs, 
 # PCREC = Path.home() / 'Documents' / 'ShareX' / 'Screenshots'        #Contains: 
@@ -23,6 +20,9 @@ ARTIFACTS = Path.home() / 'Artifacts'                               #ARTIFACT DE
 MOVEMENT = True
 NOW = d.today()
 
+# CONFIG
+logging.basicConfig(filename=ARTIFACTS / 'Runner.log', level=logging.DEBUG, format='%(asctime)s  [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 # UTILS:
 # RENAME TO SUBJECT_RETURN_FROM_PARAMETER
 month_str = lambda d: d.strftime(r'%Y-%m')
@@ -30,7 +30,7 @@ unix_time = lambda e: dt.fromtimestamp(int(e)).strftime(r'%H-%M-%S')
 unix_date = lambda e: dt.fromtimestamp(int(e)).strftime(r'%Y-%m-%d')
 ctime_dff = lambda f: dt.fromtimestamp(f.stat().st_ctime).date().strftime(r'%d')
 ctime_tff = lambda f: dt.fromtimestamp(f.stat().st_ctime).time().strftime(r'%H-%M-%S')
-mtime_dff = lambda f: dt.fromtimestamp(f.stat().st_mtime).date()
+cdate_dff = lambda f: dt.fromtimestamp(f.stat().st_ctime).date()
 mtime_tff = lambda f: dt.fromtimestamp(f.stat().st_mtime).time().strftime(r'%H-%M-%S')
 
 resolve_u = lambda m: unix_time(m.group(0))
@@ -42,7 +42,10 @@ file_exists = lambda f, n: (f.parent / f'{n}{f.suffix}').exists() or (ARTIFACTS 
 files = lambda dir: filter(lambda f: not f.is_dir(), dir.iterdir())
 dirs = lambda dir: filter(lambda f: f.is_dir(), dir.iterdir())
 month_dirs = lambda dir: filter(lambda d: re.fullmatch(r'^\d\d\d\d-\d\d$', d.name), dir)
+year_dirs = lambda dir: filter(lambda d: re.fullmatch(r'^\d\d\d\d$', d.name), dir)
 ignore_marked = lambda dir: filter(lambda f: '[x]' not in f.stem, dir)
+renamed_files = lambda dir: filter(lambda f: re.match(r'\[[A-Za-z]{3}\] [A-Z]{2}', f.stem), dir)
+
 
 #DERIVATIVE:
 THIS_MONTH = month_str(NOW)
@@ -59,9 +62,10 @@ class Artifact:
         self.named = rf'^{replace[:8]}'
 
     def identify(self, file: Path) -> bool:
-        logging.debug(f'Identifying the file {file.name} as {self.named}')
         suffix = self.suffix == file.suffix.lower() if self.suffix else True
         pattern = re.fullmatch(self.pattern, file.stem, re.I) if self.pattern else True
+        if pattern: logging.debug(f'File {file.name} is identified as {self.named[1:]}')
+        else: logging.warning(f'File {file.name} is NOT identified as {self.named[1:]}')
         return bool(suffix and pattern)
 
     def new_name(self, file: Path):
@@ -70,17 +74,18 @@ class Artifact:
         name = re.sub(r'T\((\d\d)[ .]?(\d\d)[ .]?(\d\d)\)', r'T(\1-\2-\3)', name)
         name = re.sub(r'T\(\)', f'T({ctime_tff(file)})', name)
         name = re.sub(r' T\(', f' {ctime_dff(file)}T(', name)
+        name = name.strip(' -')
         while file_exists(file, name): name += '.'
-        logging.debug(f'New name is evaluated to {name}')
+        logging.debug(f'New name is {name}')
         return name
 
     def rename(self, file: Path, check=False, move=False):
         if not check or self.identify(file):
+            logging.info(f'Renaming {file}')
             new_name = self.new_name(file)
             new_path = (ARTIFACTS if move else file.parent) / f'{new_name}{file.suffix}'
-            logging.debug(f'Renaming {file}')
-            logging.info(f'New name {new_path}')
-            #file = file.rename(new_path)
+            logging.info(f'Ranamed  {new_path}')
+            file = file.rename(new_path)
         return file
         
 #ARTIFACT OBJECTS
@@ -95,12 +100,14 @@ AndN_RC = Artifact(r'^screen-(?P<Date>[0-9]{8})-(?P<Time>[0-9]{6})$', r'[And] RC
 And_LG1 = Artifact(r'^(?P<Name>.*)$', r'[And] LG T() \g<Name>', r'.zip')
 And_LG2 = Artifact(r'^Logcat file From Android Device(?P<Info>.*)$', r'[And] LG T() \g<Info>.zip', r'')
 
-iPad_LG = Artifact(r'^(?P<Date>[0-9-]{10}) (?P<Time>[0-9]{6})$', r'[iPd] LG T(\g<Time>)', r'.zip')
+iPad_LG = Artifact(r'^(?P<Info>.*)(?P<Date>[0-9-]{10}) (?P<Time>[0-9]{6})$', r'[iPd] LG T(\g<Time>) \g<Info>', r'.zip')
 Web_HAR = Artifact(r'^(?P<Info>.*)my(?P<Env>[a-z]+)?\.arlo\.com$', r'[Web] HR T() \g<Env> \g<Info>', r'.har')
 Web_LOG = Artifact(r'^(?P<Info>.*)my(?P<Env>[a-z]+)?\.arlo\.com-(?P<Time>[0-9]{13})$', r'[Web] LG T() \g<Env> \g<Info>', r'.log')
 
 Web_SSH = Artifact(r'^(?P<App>[A-Za-z0-9]+)_[a-zA-Z0-9]{10}$', r'[Web] SS T() \g<App>', r'.png')
 Web_REC = Artifact(r'^(?P<App>[A-Za-z0-9]+)_[a-zA-Z0-9]{10}$', r'[Web] RC T() \g<App>', r'.mp4')
+
+Web_UNI = Artifact(r'^(?P<UN>.*)$', r'[Web] UN T() \g<UN>', r'')
 
 
 def zip_is_android_logs(file: Path) -> bool:
@@ -123,7 +130,7 @@ def move_files(dir1, dir2):
 def artifact_collection_drive():
     logging.info('Starting scan of the Google Drive Folder')
     for file in ignore_marked(files(GDRIVE)):
-        match file.suffix:
+        match file.suffix.lower():
             case '':
                 if zip_is_android_logs(file): file = And_LG2.rename(file, move=MOVEMENT)
                 else: file = file.rename(file.parent / f'{file.stem}[x]{file.suffix}')
@@ -138,7 +145,7 @@ def artifact_collection_drive():
 def artifact_collection_download():
     logging.info('Starting scan of the Downloads Folder')
     for file in ignore_marked(files(DLDIR)):
-        match file.suffix:
+        match file.suffix.lower():
             case '.zip': file = check_for(file, iPad_LG)
             case '.har': file = check_for(file, Web_HAR)
             case '.log': file = check_for(file, Web_LOG)
@@ -148,37 +155,59 @@ def artifact_collection_sharex():
     logging.info('Starting scan of the ShareX subfolders')
     for directory in month_dirs(dirs(ARTIFACTS)):
         for file in ignore_marked(files(directory)):
-            match file.suffix:
+            match file.suffix.lower():
                 case '.png': file = check_for(file, Web_SSH)
                 case '.mp4': file = check_for(file, Web_REC)
 
-def group_by_date():
+def return_renamed_files():
+    logging.info('Engaging debug fuction of collecting all the files from fubfolders back to the root')
+    for directory in month_dirs(dirs(ARTIFACTS)):
+        for file in renamed_files(files(directory)):
+            logging.debug(f'Moving {file.name} from subfolder {directory.name} to the Artifacts root')
+            file.rename(ARTIFACTS / file.name)
+
+def older_to_monthly_subfolders():
     logging.info('Starting the scan of files in the Artifact folder, older files will be moved to the subfolders')
     for file in files(ARTIFACTS):
-        date = ctime_dff(file)
+        if file == 'Runner.log': continue
+        date = cdate_dff(file)
         if (NOW - date).days > 5:
-            ensure_dir_exists(ARTIFACTS / month_str(date))
-            file.rename(ARTIFACTS / month_str(date) / file.name)
-    
-    logging.info('Starting the scan of directories in the Artifact folder, all but two latest will be moved to the yearly folders')
+            month = month_str(date)
+            ensure_dir_exists(ARTIFACTS / month)
+            logging.info(f'Moving {file.name} to folder {month}')
+            file.rename(ARTIFACTS / month / file.name)
+
+def older_to_yearly_subfolders():
+    logging.info('Starting the scan of folders in the Artifact folder, all but two latest will be moved to the yearly folders')
     for directory in month_dirs(dirs(ARTIFACTS)):
         if directory.name in (THIS_MONTH, PREV_MONTH): continue
         move_files(directory, path_with_year(directory))
         directory.rmdir()
 
+def return_monthly_subfolders():
+    logging.info('Engaging debug fuction of collecting all monthly subfolders back to the root')
+    for directory in year_dirs(dirs(ARTIFACTS)):
+        for subfolder in month_dirs(dirs(directory)):
+            move_files(subfolder, ARTIFACTS / subfolder.name)
+            subfolder.rmdir()
+
+def main():
+    logging.info('Starting the Script')
+    logging.info('Files will be moved to the Artifact folder' if MOVEMENT else 'Files will be renamed in place')
+
+    #artifact_collection_drive()
+    #artifact_collection_download()
+    #artifact_collection_sharex()
+    #older_to_monthly_subfolders()
+    #older_to_yearly_subfolders()
+    
+    #return_monthly_subfolders()
+    #return_renamed_files()
+
     # ADD - ARCHIVING OLD HARS, RESIZING OLD PICS
 
     # ADD - ARCHIVING OLD YEARS
 
-def main():
-    # PRINT OUT LOGS
-    logging.info('Starting the Script')
-    logging.info('Files will be moved to the Artifact folder' if MOVEMENT else 'Files will be renamed in place')
-
-    artifact_collection_drive()
-    #artifact_collection_download()
-    #artifact_collection_sharex()
-    #group_by_date()
 
 if __name__ == '__main__':
     main()
